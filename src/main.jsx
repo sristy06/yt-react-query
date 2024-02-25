4b7506ec61f6e9af1f2a021b24712775d3f70607
sristy06 <73926465+sristy06@users.noreply.github.com>
2024-02-25 22:04:35 +0530

    Fetch Api

--- src/main.jsx
+++ src/main.jsx
@@ -23,14 +23,7 @@ const router = createBrowserRouter([
     },
 ]);
 
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 10000,
-        },
-    },
-});
-
+const queryClient=new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
